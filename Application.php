e7763dd7e489c3e56679e653eb58fb773e10164a
Makoto <FujiMakoto@users.noreply.github.com>
2015-11-13 03:33:01 -0500

    Initial commit

---
+++ Application.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @brief		Advanced Favicons Application Class
+ * @author		<a href='https://www.Makoto.io'>Makoto Fujimoto</a>
+ * @copyright	(c) 2015 Makoto Fujimoto
+ * @package		IPS Social Suite
+ * @subpackage	Advanced Favicons
+ * @since		13 Nov 2015
+ * @version		
+ */
+ 
+namespace IPS\favicons;
+
+/**
+ * Advanced Favicons Application Class
+ */
+class _Application extends \IPS\Application
+{
+
+}
